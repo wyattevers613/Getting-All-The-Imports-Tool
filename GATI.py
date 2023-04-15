b0574acc5fd9c26f28952f5581119333c530f132
wyattevers613 <wyattevers613@gmail.com>
2023-04-14 23:45:08 -0400

    combined Tool with future integration areas with other tools included

--- GATI.py
+++ GATI.py
@@ -19,20 +19,28 @@ if not os.path.isfile(file_path):
 print("\n")
 print("Welcome to the GATI tool!")
 print("\n")
+
+# Embedded and UPX unpacking
+# print out formatted results
+# 
+# print("\n")
+
+# Entropy / Delay
 # # run check entropy with checkEntropy(path) and print out result
 # entropy_result = checkEntropy(file_path)
 # print(f"Entropy result: {entropy_result}")
-
+# print("\n")
 # # run checkDelay(path) and print out result
 # delay_result = checkDelay(file_path)
 # print(f"Delay check result: {delay_result}")
+# print("\n")
 
 # run IAT_Extraction.extract(path), it will print out results on its own
-print("Visible Import Address Table")
+print("Visible Import Address Table...")
 print("\n")
 iat_extract(file_path)
 print("\n")
-print("Hidden Libraries and Functions")
+print("Hidden Libraries and Functions...")
 # run the delay load checker and print out results
 libraries, functions = find_loadlibrarya_getprocaddress_calls(file_path)
 print("\n")
