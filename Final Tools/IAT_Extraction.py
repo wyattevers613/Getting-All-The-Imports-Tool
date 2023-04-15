3dced0da36dc1c57f40d5b6c8f719693c17dc103
wyattevers613 <wyattevers613@gmail.com>
2023-04-14 23:10:57 -0400

    reorganized repo

---
+++ Final Tools/IAT_Extraction.py
@@ -0,0 +1,31 @@
+import pefile
+
+# specify the path of the executable
+pe_file = "C:\\Users\\user\\Desktop\\Project\\meoware.exe"
+
+
+# grab existing IAT
+
+# load the executable into pefile
+pe = pefile.PE(pe_file)
+
+# get the Import Address Table
+iat = pe.DIRECTORY_ENTRY_IMPORT
+
+libraries_count = 0
+function_count = 0
+
+# print out the IAT
+print("Import Address Table:")
+for entry in iat:
+    libraries_count += 1
+    print("    " + entry.dll.decode())
+    """
+    for imp in entry.imports:
+        function_count += 1
+        print("        " + hex(imp.address) + " " + imp.name.decode())
+    """
+
+print(f"Total Libraries Imported: {libraries_count}")
+#print(f"Total Functions Imported: {function_count}")
+
---
+++ Misc Work/Delay_Loaded_Library_Extraction.py
@@ -0,0 +1,62 @@
+import pefile
+import capstone
+import re
+
+file_path = "C:\\Users\\user\\Desktop\\Project\\meoware.exe"
+
+def find_loadlibrarya_calls(file_path):
+    pe = pefile.PE(file_path)
+
+    for section in pe.sections:
+        if b".text" in section.Name:
+            text_section = section
+            break
+    else:
+        print("No .text section found.")
+        return
+
+    cs = capstone.Cs(capstone.CS_ARCH_X86, capstone.CS_MODE_32)
+
+    loadlibrarya_address = None
+    for entry in pe.DIRECTORY_ENTRY_IMPORT:
+        if entry.dll.lower() == b"kernel32.dll":
+            for imp in entry.imports:
+                if imp.name and imp.name.decode().lower() == "loadlibrarya":
+                    loadlibrarya_address = imp.address
+                    break
+            if loadlibrarya_address:
+                break
+    if not loadlibrarya_address:
+        print("LoadLibraryA not found in import table.")
+        return
+
+    disassembly = cs.disasm(text_section.get_data(), text_section.VirtualAddress)
+
+    loaded_libraries = []
+
+    for instruction in disassembly:
+        hex_value_match = re.search(r'0x[0-9a-fA-F]+', instruction.op_str)
+        if instruction.mnemonic == "call" and hex_value_match and int(hex_value_match.group(0), 16) == loadlibrarya_address:
+            print(f"LoadLibraryA call found at: 0x{instruction.address:x}")
+
+            for push_instruction in reversed(list(cs.disasm(text_section.get_data()[:instruction.address - text_section.VirtualAddress], text_section.VirtualAddress))):
+                if push_instruction.mnemonic == "push":
+                    library_address = int(push_instruction.op_str, 16)
+                    library_name = pe.get_string_at_rva(library_address - pe.OPTIONAL_HEADER.ImageBase)
+                    loaded_libraries.append(library_name.decode())
+                    print(f"  Loading library: {library_name}")
+                    break
+
+    print("\nLoaded libraries:")
+    for library in loaded_libraries:
+        print(library)
+
+if __name__ == "__main__":
+    # import sys
+
+    # if len(sys.argv) < 2:
+    #     print("Usage: python find_loadlibrarya.py <path_to_exe_file>")
+    #     sys.exit(1)
+
+    # exe_file_path = sys.argv[1]
+    find_loadlibrarya_calls(file_path)
